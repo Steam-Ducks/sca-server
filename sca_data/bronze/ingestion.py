09077f7b2e277d079929c86790a8f93e0073d05c
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2026-03-23 21:20:13 -0300

    Implement the Data Extraction from API (#4)
    * build(SCA-36): add sqlalchemy to requirements.txt
    
    * feat(SCA-36): create staging tables ingestion
    
    * fix(SCA-36): fix lint error
    
    * fix(SCA-36): fix black errors

---
+++ sca_data/bronze/ingestion.py
@@ -0,0 +1,60 @@
+from sca_data.db.connection import getOrCreate
+import pandas as pd
+import requests
+import logging
+import datetime
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+)
+
+ENGINE = getOrCreate()
+ENDPOINT = "https://sca-api-sb1c.onrender.com/"
+
+
+def _create_table(df: pd.DataFrame, engine, tb_name: str):
+    logging.info(f"Writing {tb_name} ...")
+
+    df["bronze_ingested_at"] = datetime.datetime.now()
+
+    df.to_sql(
+        tb_name,
+        engine,
+        schema="bronze",
+        if_exists="replace",
+        index=False,
+    )
+
+    logging.info(f"Table {tb_name} wrote in schema 'bronze'!")
+
+
+def _build_df(endpoint: str, file: str) -> pd.DataFrame:
+    try:
+        response = requests.get(f"{endpoint}/files/{file}")
+        df = pd.DataFrame(response.json())
+
+        logging.info(f"Dataframe built to file {file}.")
+
+        _create_table(df, ENGINE, file.replace(".parquet", ""))
+
+    except Exception as e:
+        logging.error(f"It was not possible to ingest file {file}. Error {e}")
+
+
+def _make_request(endpoint: str):
+    try:
+        response = requests.get(f"{endpoint}/files")
+
+        if response.status_code == 200:
+            for item in response.json():
+                _build_df(endpoint, item)
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error: {e}")
+        return []
+
+
+_make_request(ENDPOINT)
---
+++ sca_data/db/connection.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from dotenv import load_dotenv
+
+
+def getOrCreate() -> Engine:
+
+    load_dotenv()
+
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = "localhost"
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+
+    connection_string = (
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    )
+
+    engine = create_engine(connection_string)
+
+    return engine
