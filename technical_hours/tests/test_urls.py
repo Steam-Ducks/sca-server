87c8b43fcc842d1d3df44c1bbfe5a1a3c29539c7
Samuel Prado <163088005+Samuelprado99@users.noreply.github.com>
2026-04-21 02:09:15 -0300

    #SCA-243 - Implement filter endpoint by period on technical hours (#16)
    * feat(SCA-243): implement filter endpoint by period on technical hours dashboard
    
    * feat(SCA-243): implement filter endpoint by period on technical hours dashboard
    
    * feat(SCA-243): implement filter endpoint by period on technical hours dashboard

--- technical_hours/tests/test_urls.py
+++ technical_hours/tests/test_urls.py
@@ -1,10 +1,18 @@
 from django.urls import resolve, reverse
 
-from technical_hours.views import TechnicalHoursTableView
+from technical_hours.views import (
+    TechnicalHoursTablePeriodoView,
+    TechnicalHoursTableView,
+)
 
 
 def test_technical_hours_url_resolve():
     url = reverse("technical-hours-table")
     resolver = resolve(url)
-
     assert resolver.func.view_class == TechnicalHoursTableView
+
+
+def test_technical_hours_periodo_url_resolve():
+    url = reverse("technical-hours-table-periodo", kwargs={"periodo": "2024-03"})
+    resolver = resolve(url)
+    assert resolver.func.view_class == TechnicalHoursTablePeriodoView
