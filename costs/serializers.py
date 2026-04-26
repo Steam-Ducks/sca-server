88b35c4d4efef68fb93e5c601f725bad5c51472a
Felipe Reis <felipereisdebrito@gmail.com>
2026-04-26 19:59:10 -0300

    feat(SCA-273): serializer accepts date

--- costs/serializers.py
+++ costs/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from sca_data.models import GoldCosts
+from datetime import datetime, time
 
 
 class GoldCostsSerializer(serializers.ModelSerializer):
+    data = serializers.DateField()
+
     class Meta:
         model = GoldCosts
         fields = "__all__"
+
+    def to_internal_value(self, data):
+        value = super().to_internal_value(data)
+
+        if "data" in value:
+            value["data"] = datetime.combine(value["data"], time.min)
+
+        return value
