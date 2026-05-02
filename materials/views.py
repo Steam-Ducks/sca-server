37775cdb87b393a9d9c199109e7f4874526824c0
Alexander Lima <lima.alexander.las@gmail.com>
2026-05-02 15:34:37 -0300

    fix(SCA-242): fix black check on CI test

--- materials/views.py
+++ materials/views.py
@@ -6,7 +6,10 @@ from rest_framework import generics
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from materials.serializers import MaterialsIndicatorsSerializer, MaterialsTableSerializer
+from materials.serializers import (
+    MaterialsIndicatorsSerializer,
+    MaterialsTableSerializer,
+)
 from sca_data.models import (
     SilverMaterial,
     SilverPedidoCompra,
