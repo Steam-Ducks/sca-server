cbdbbb55eb7db7a43be9c5294d85c9e667791c53
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2026-05-21 01:36:16 -0300

    feat(SCA-322): add audit app views, serializers, and url routing

--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("api/", include("budget.urls")),
     path("api/", include("imports.urls")),
     path("api/monitoring/", include("monitoring.urls")),
+    path("api/", include("audit.urls")),
 ]
