72c7c6784a7856d0cea97b00f97f9ac7980c70cd
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2026-05-22 00:41:49 -0300

    fix(SCA-322): register core urls and allow unauthenticated unit tests

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -12,6 +12,7 @@ from users.serializers import LoginSerializer, UserSerializer
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LoginView(APIView):
