afe4409a673f10a23d5a997c95e36522318589c2
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2026-05-22 01:01:26 -0300

    fix(SCA-322): enforce required and unique email in user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,16 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, value):
+        qs = User.objects.filter(email=value)
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
+            raise serializers.ValidationError("Já existe um usuário com este e-mail.")
+        return value
+
     class Meta:
         model = User
         fields = ["id", "username", "name", "email"]
